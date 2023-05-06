a894412023715f5b54346c062660c21985224968
luke-weller <wellerluke@hotmail.com>
2023-05-06 22:34:02 +0100

    #2 add the help command

--- src/commands.js
+++ src/commands.js
@@ -1,6 +1,9 @@
 import { ClearCommand } from './commands/clear.js';
 import { EchoCommand } from './commands/echo.js';
+import { HelpCommand } from './commands/help.js';
+
 
 export const commands = new Map();
 commands.set('clear', new ClearCommand());
 commands.set('echo', new EchoCommand());
+commands.set('help', new HelpCommand(commands));
